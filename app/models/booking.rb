8a19728096da460a7e553ca508becd23d8db7a06
Oceane Delain <delain.oceane@gmail.com>
2021-05-18 18:24:37 +0200

    not declined and accepted but totalprice

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,10 @@ class Booking < ApplicationRecord
 
   validates :start_date, :end_date, presence: true
   validates :status, inclusion: { in: ["Waiting", "Accepted", "Declined"] }
+
+  before_save :set_total_price
+
+  def set_total_price
+    self.totalprice = plant.price * (Booking.last.end_date - Booking.last.start_date).to_i
+  end
 end
